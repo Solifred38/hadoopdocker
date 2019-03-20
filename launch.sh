4d93cb59ad83bad14fbbeb0d73e02f4940b37720
Frederic Fromager <fred.fromager@gmail.com>
2019-03-20 16:55:23 +0100

    logs
    add logs

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #stop du dernier conteneur
 docker stop hadoop
-docker run --rm -P -d  -v $PWD/tp:/tp --name hadoop hadoop:fred /etc/bootstrap.sh -d
+docker run --rm -P -d  -v $PWD/tp:/tp -v $PWD/logs:/usr/local/hadoop-2.7.0/logs/userlogs --name hadoop hadoop:fred /etc/bootstrap.sh -d
+chown -R fromager:fromager $PWD/logs
 sleep 5
 docker exec -t hadoop hadoop dfsadmin -safemode leave
 docker exec -t hadoop ./start.sh
