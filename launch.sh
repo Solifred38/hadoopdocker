d1c6ea85ea8fae273a4319d45e7f33a1711f7805
Frederic Fromager <fred.fromager@gmail.com>
2019-03-21 15:06:57 +0100

    retour du sleep dans lanch

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #stop du dernier conteneur
 docker stop hadoop
 docker run --rm -P -d  -v $PWD/tp:/tp -v $PWD/logs:/usr/local/hadoop-2.7.0/logs/userlogs --name hadoop hadoop:fred /etc/bootstrap.sh -d
-#sleep 2
+sleep 2
 docker exec -t hadoop hdfs dfsadmin -safemode leave
 docker exec -t hadoop ./start.sh
